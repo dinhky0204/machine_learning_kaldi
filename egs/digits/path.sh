abc7ee351a0d4bc34cef6099fcd4baac3099f2c0
Dinh Ky <dinhky0204@gmail.com>
2018-03-04 09:51:50 +0700

    demo traning model

---
+++ egs/digits/path.sh
@@ -0,0 +1,11 @@
+export KALDI_ROOT=`pwd`/../.. # Setting paths to useful tools
+export PATH=$PWD/utils/:$KALDI_ROOT/src/bin:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/src/fstbin/:$KALDI_ROOT/src/gmmbin/:$KALDI_ROOT/src/featbin/:$KALDI_ROOT/src/lm/:$KALDI_ROOT/src/sgmmbin/:$KALDI_ROOT/src/sgmm2bin/:$KALDI_ROOT/src/fgmmbin/:$KALDI_ROOT/src/latbin/:$KALDI_ROOT/src/lmbin/:$PWD:$PATH
+
+# Defining audio data directory (modify it for your installation directory!)
+export DATA_ROOT="/home/dinhky/Do_An/kaldi-trunk/egs/digits/digits_audio"
+
+# Variable that stores path to MITLM library
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)/tools/mitlm-svn/lib
+
+# Variable needed for proper data sorting
+export LC_ALL=C
